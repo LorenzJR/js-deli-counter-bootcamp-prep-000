167926bbe8f863a60730a780a6596ad5899ead55
Lorenz Jahn <jahnl@mskcc.org>
2018-02-17 19:23:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+var katzDeli = []
+function takeANumber(array, name){
+  array.push(name);
+  return  `Welcome, ${name}. You are number ${array.length} in line.`
+}
+
+function nowServing(array){
+  
+}
+
+function currentLine(array){
+  
+}
