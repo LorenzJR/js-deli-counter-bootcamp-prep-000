66b3d0355760b6eadeb5945713c52171250014c3
Lorenz Jahn <jahnl@mskcc.org>
2018-02-17 20:21:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ function nowServing(array){
 
 function currentLine(array){
   if (array.length > 0){
-    var line = "The line is currently: "
+    var output = [] 
     for (let i = 0; i < array.length; i++){
       var x = i + 1;
-      line = line + x + ". " + array[i] + ", "
+     output.push(`${x}. ${array[i]}`)
       }
-    return `${line}`
+    return " The line is currently: " + output.toString()
   } else {
     return "The line is currently empty."
   }
